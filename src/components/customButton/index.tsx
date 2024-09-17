c3b9e81ef4c5c4448ebdbd14b52adcb03cafcee5
unknown <d.kolodinskaya02@gmail.com>
2024-09-17 16:57:10 +0300

    style: create a shared theme

--- src/components/customButton/index.tsx
+++ src/components/customButton/index.tsx
@@ -1,16 +1,11 @@
 import { ReactNode } from "react";
 
-import styles from "./index.module.css";
-
 type Props = {
   children: ReactNode;
   action?: () => void;
 };
 
 export const CustomButton = ({ children, action }: Props) => {
-  return (
-    <button className={styles.btn} onClick={action}>
-      {children}
-    </button>
-  );
+  return <button onClick={action}>{children}</button>;
 };
+
